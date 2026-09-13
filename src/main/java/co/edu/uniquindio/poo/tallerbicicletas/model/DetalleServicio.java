a91e43ccc2b0e3cb3a146cb8a6e25ecd0b8acc83
majo mejia <mariaj.mejiag2@uqvirtual.edu.co>
2026-09-13 18:15:53 -0500

    realice la clase DetalleServicio y el num de TipoDetalle

---
+++ src/main/java/co/edu/uniquindio/poo/tallerbicicletas/model/DetalleServicio.java
@@ -0,0 +1,69 @@
+package co.edu.uniquindio.poo.tallerbicicletas.model;
+
+public class DetalleServicio {
+
+    private String descripcion;
+    private TipoDetalle tipo;
+    private int cantidad;
+    private double costoUnitario;
+    private Repuesto repuesto;
+
+    public DetalleServicio(String descripcion, TipoDetalle tipo, int cantidad, double costoUnitario, Repuesto repuesto) {
+        this.descripcion = descripcion;
+        this.tipo = tipo;
+        this.cantidad = cantidad;
+        this.costoUnitario = costoUnitario;
+        this.repuesto = repuesto;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public TipoDetalle getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoDetalle tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public double getCostoUnitario() {
+        return costoUnitario;
+    }
+
+    public void setCostoUnitario(double costoUnitario) {
+        this.costoUnitario = costoUnitario;
+    }
+
+    public Repuesto getRepuesto() {
+        return repuesto;
+    }
+
+    public void setRepuesto(Repuesto repuesto) {
+        this.repuesto = repuesto;
+    }
+
+    @Override
+    public String toString() {
+        return "DetalleServicio{" +
+                "descripcion='" + descripcion + '\'' +
+                ", tipo=" + tipo +
+                ", cantidad=" + cantidad +
+                ", costoUnitario=" + costoUnitario +
+                ", repuesto=" + repuesto +
+                '}';
+    }
+}
