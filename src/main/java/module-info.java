95c45f930bea3bbdd2ebf1e8d9b8a1be527a71f1
majo mejia <mariaj.mejiag2@uqvirtual.edu.co>
2026-09-14 00:56:38 -0500

    interfaz grafica casi terminada, faltaba arreglar el precio unitario porque no aparece

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,9 +2,15 @@ module co.edu.uniquindio.poo.tallerbicicletas {
     requires javafx.controls;
     requires javafx.fxml;
 
+    // Exportar paquetes para acceso directo
+    exports co.edu.uniquindio.poo.tallerbicicletas;
+    exports co.edu.uniquindio.poo.tallerbicicletas.controllers;
+    exports co.edu.uniquindio.poo.tallerbicicletas.model;
+    exports co.edu.uniquindio.poo.tallerbicicletas.modell;
 
+    // Abrir paquetes para reflexión (Tablas y FXML)
     opens co.edu.uniquindio.poo.tallerbicicletas to javafx.fxml;
     opens co.edu.uniquindio.poo.tallerbicicletas.controllers to javafx.fxml;
-    opens co.edu.uniquindio.poo.tallerbicicletas.model to javafx.base;
-    exports co.edu.uniquindio.poo.tallerbicicletas;
+    opens co.edu.uniquindio.poo.tallerbicicletas.model to javafx.base, javafx.fxml;
+    opens co.edu.uniquindio.poo.tallerbicicletas.modell to javafx.base, javafx.fxml;
 }
